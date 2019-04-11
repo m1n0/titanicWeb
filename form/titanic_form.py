c29fa67da3fa864c51c7d4aefa849779022d77fc
m1n0 <mino.lukac@gmail.com>
2019-04-12 00:53:07 +0200

    Form definition

---
+++ form/titanic_form.py
@@ -0,0 +1,117 @@
+from wtforms import Form, TextField, validators, SubmitField, DecimalField, IntegerField, SelectField
+
+class TitanicForm(Form):
+    sex = SelectField(
+        "Sex:",
+        choices = [
+            ("1", "Male"),
+            ("0", "Female")
+        ],
+        validators = [
+            validators.InputRequired()
+        ]
+    )
+
+    title = SelectField(
+        "Title:",
+        choices = [
+            ("Mr", "Mr"),
+            ("Miss", "Miss"),
+            ("Mrs", "Mrs"),
+            ("Master", "Master")
+        ],
+        validators = [
+            validators.InputRequired()
+        ]
+    )
+
+    age = DecimalField(
+        "Age:",
+        default = 30,
+        places = 0,
+        validators = [
+            validators.InputRequired(),
+            validators.NumberRange(
+                min = 0.5,
+                max = 80,
+                message = "Age must be between 0.5 and 80"
+            )
+        ]
+    )
+
+    Pclass = SelectField(
+        "Ticket class:",
+        choices = [
+            ("1", "First Class"),
+            ("2", "Second Class"),
+            ("3", "Third Class")
+        ],
+        validators = [
+            validators.InputRequired()
+        ]
+    )
+
+    cabin = SelectField(
+        "Has cabin or not:",
+        choices = [
+            ("1", "Yes"),
+            ("0", "No")
+        ],
+        validators = [
+            validators.InputRequired()
+        ]
+    )
+
+    SibSp = IntegerField(
+        "Number of siblings and spouses aboard:",
+        default = 0,
+        validators = [
+            validators.InputRequired(),
+            validators.NumberRange(
+                min = 0,
+                max = 9,
+                message="Number must be between 0 and 9"
+            )
+        ]
+    )
+
+    ParCh = IntegerField(
+        "Number of parents and children aboard:",
+        default = 0,
+        validators = [
+            validators.InputRequired(),
+            validators.NumberRange(
+                min = 0,
+                max = 9,
+                message = "Number must be between 0 and 9"
+            )
+        ]
+    )
+
+    fare = DecimalField(
+        "Passenger Fare:",
+        default = 33,
+        places = 1,
+        validators = [
+            validators.InputRequired(),
+            validators.NumberRange(
+                min = 0,
+                max = 512,
+                message = "Fare must be between 0 and 512"
+            )
+        ]
+    )
+
+    embarked = SelectField(
+        "Port of Embarkation:",
+        choices = [
+            ("S", "Southampton, England"),
+            ("C", "Cherbourg, France"),
+            ("Q", "Queenstown, Ireland")
+        ],
+        validators = [
+            validators.InputRequired()
+        ]
+    )
+
+    submit = SubmitField("Predict")
